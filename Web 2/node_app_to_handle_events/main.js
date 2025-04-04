8a011500ed5a8b45e99d20b663edc8d4b0d72422
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-04 19:12:02 +0300

    lemme try this event handling thing on nodejs

--- Web 2/node_app_to_handle_events/main.js
+++ Web 2/node_app_to_handle_events/main.js
@@ -1,6 +1,4 @@
 const events = require('events');
-const http = require('http');
-
 const eventEmitter = new events.EventEmitter;
 
 function myEventHandler(){
@@ -9,9 +7,4 @@ function myEventHandler(){
 
 eventEmitter.on('click', myEventHandler);
 
-// eventEmitter.emit('click');
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-type':'text/html'});
-    res.write('<h1>This is a page to display a callback function thingy</h1>')    
-}).listen(8080);
+eventEmitter.emit('click');
